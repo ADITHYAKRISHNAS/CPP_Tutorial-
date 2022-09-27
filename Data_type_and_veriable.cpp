1dc59da867051191620e41b84199c79fd0e9e564
Adithya Krishna S <adithyakrishnas2015@gmal.com>
2022-09-27 15:38:14 +0530

    Datatype and veriables

---
+++ Data_type_and_veriable.cpp
@@ -0,0 +1,41 @@
+// // Your First C++ Program
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string messages = "Hello World";
+    
+    cout << messages << endl;
+    cout << "Message is " << messages<< endl;
+
+    int number = 1000;
+    cout << number << endl;
+
+    float floatNumber = 1.36;
+    cout<<floatNumber<<endl;
+    
+    double dbNumber = 2.9878888;
+    cout<< dbNumber << endl;
+
+    bool cpgm =true;
+    cout<<cpgm<<endl;
+
+    char charText = 'A';
+    cout<< charText<<endl;
+    return 0;
+}
+
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     string messages ;
+//     messages = "Hello";
+    
+//     cout << messages << endl;
+//     cout << "Message is " << messages<< endl;
+//     return 0;
+// }
