0d094e5384d67bfcf308b1b664d0fd7cfa80225f
Adithya Krishna S <adithyakrishnas2015@gmal.com>
2022-09-30 23:52:28 +0530

    Operator concept is added

---
+++ Operators.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+using namespace std;
+
+// int main()
+// {
+//     int result, n1=20, n2=7;
+//     // Addition
+//     // result = n1 + n2;
+//     //  Subraction
+//     // result = n1 - n2;
+//     // Multilication
+//     // result = n1 * n2;
+//     // Division
+//     // result = n1 / n2;
+//     // Remainter
+//     result = n1 % n2;
+
+//     cout << result << endl;
+//     return 0;
+// }
+
+// int main()
+// {
+//     int result=5;
+//     // result ++ ; // result = result + 1 
+//     result -- ; // result = result - 1 
+
+//     cout << result << endl;
+//     return 0;
+// }
+
+// int main()
+// {
+//     int x = 10;
+//     x+=4;
+//     x-=4;
+//     x*=4;
+//     x/=4;
+//     x%=4;
+//     cout << x <<endl;
+//     return 0;
+// }
+
+// int main()
+// {
+//     int a=15,b=10,compare;
+//     // compare = a==b;
+//     // compare = a!=b;
+//     // compare = a>b;
+//     // /compare = a<b;
+//     // /compare = a<=b;
+//     // /compare = a>=b;
+
+//     cout<<compare<<endl;
+//     // cout<<(a>=b)<<endl;
+
+// }
+
+int main()
+{
+    int a= 10,b=15;
+
+    // cout<<(a<b && a>5) << endl; 
+    // cout<<(a<b || a>20) << endl; 
+    cout<<((!(a>5))) << endl; 
+
+    return 0;
+
+}
